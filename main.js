0a6bc6d44dea8ef254a069c59137234785520e51
Manuel Hernández <manuel.jhernandez.91@me.com>
2018-07-03 18:16:17 +0100

    Finished exercise

--- main.js
+++ main.js
@@ -4,3 +4,27 @@ let isDown = false;
 let startX;
 let scrollLeft;
 
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', (e) => {
+  if(!isDown) return; //Stop function from running if click not active
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 2;
+  slider.scrollLeft = scrollLeft - walk;
+});
